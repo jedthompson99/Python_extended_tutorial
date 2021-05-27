79ccc6366cb554314aa6aab3a375d821abbdab46
Jed Thompson <jedthompson@gmail.com>
2021-05-27 07:52:52 -0600

    Django Website 1

---
+++ checkpoint_M2CP3.py
@@ -0,0 +1,44 @@
+import math
+
+# Task 1
+list = ['Mary', 'John', 'Tuesday', 'Summer']
+tuple = (2, 3, 4, 5)
+float = 2.543
+integer = 2
+decimal = 0.5
+dictionary = {
+    'name': 'John',
+    'age': 35,
+    'bald': True,
+    'viable': False}
+
+# Task 2
+float_round = round(float, 2)
+print(float_round)
+
+# Task 3
+result = math.sqrt(15.4)
+print(result)
+
+# Task 4
+variable = dictionary.get("name")
+print(variable)
+
+print(dictionary['name'])
+
+# Task 5
+print(tuple[1])
+
+# Task 6
+list.append('Winter')
+print(list)
+
+# Task 7
+list.insert(0, 'Susan')
+print(list)
+
+# Task 8
+list.sort()
+print(list)
+
+# Task 9
